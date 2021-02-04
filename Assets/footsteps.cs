5c5cbed6ff6aee77c29207757b73bbc0853ac8aa
ProkhorEfimov <78382011+ProkhorEfimov@users.noreply.github.com>
2021-02-04 19:01:26 +0300

    Initial commit

---
+++ Assets/footsteps.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footsteps : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string inputsound;
+    bool playerismoving;
+    public float walkingspeed;
+}
